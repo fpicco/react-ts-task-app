405b95409ef230f779ade870049791edcaf3fdb3
fpicco <flor.picco@gmail.com>
2021-12-03 11:46:41 -0300

    baseline done

--- react-tasks-app/src/App.tsx
+++ react-tasks-app/src/App.tsx
@@ -5,27 +5,41 @@ interface ITask {
   name: string;
   done: boolean;
 }
+interface IDesc {
+  description: string;
+}
 
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
   const [tasks, setTasks] = useState<ITask[]>([]);
   const taskInput = useRef<HTMLInputElement>(null);
 
+  const [newTaskDesc, setNewTaskDesc] = useState<string>("");
+  const [tasksDesc, setTasksDesc] = useState<IDesc[]>([]);
+
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
     addTask(newTask);
+    addTaskDesc(newTaskDesc);
     setNewTask("");
+    setNewTaskDesc("");
     taskInput.current?.focus();
   };
 
   const addTask = (name: string): void => {
-    const newTasks: ITask[] = [...tasks, { name: name, done: false }];
+    const newTasks: ITask[] = [{ name: name, done: false }, ...tasks];
     setTasks(newTasks);
+  };
+  const addTaskDesc = (description: string): void => {
+    const newTasksDesc: IDesc[] = [{ description: description }, ...tasksDesc];
+    setTasksDesc(newTasksDesc);
   };
   const toggleDoneTask = (i: number): void => {
     const newTasks: ITask[] = [...tasks];
+    const newTasksDesc: IDesc[] = [...tasksDesc];
     newTasks[i].done = !newTasks[i].done;
     setTasks(newTasks);
+    setTasksDesc(newTasksDesc);
   };
   const removeTask = (i: number): void => {
     const newTasks: ITask[] = [...tasks];
@@ -48,29 +62,41 @@ function App(): JSX.Element {
                   value={newTask}
                   ref={taskInput}
                 />
+                <input
+                  className="form-control mt-2"
+                  type="text"
+                  onChange={(e) => setNewTaskDesc(e.target.value)}
+                  value={newTaskDesc}
+                  ref={taskInput}
+                />
                 <button className="btn btn-success col-12 mt-4">Save</button>
               </form>
             </div>
           </div>
 
           {tasks.map((t: ITask, i: number) => (
-            <div className="card card-body mt-2 flex flex-row justify-content-between" key={i}>
-              <h3   style={{ textDecoration: t.done ? "line-through" : "" }}>
-                {t.name}
-              </h3>
-              <div>
-                <button
-                  className="btn btn-secondary"
-                  onClick={() => toggleDoneTask(i)}
-                >
-                  {t.done ? "✓" : "✗"}
-                </button>
-                <button
-                  className="btn btn-danger  ms-3"
-                  onClick={() => removeTask(i)}
-                >
-                  🗑
-                </button>
+            <div className="card card-body mt-2" key={i}>
+              <div className="d-flex justify-content-between">
+                <h3 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                  {t.name}
+                </h3>
+                <div>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => toggleDoneTask(i)}
+                  >
+                    {t.done ? "✓" : "✗"}
+                  </button>
+                  <button
+                    className="btn btn-danger  ms-3"
+                    onClick={() => removeTask(i)}
+                  >
+                    🗑
+                  </button>
+                </div>
+              </div>
+              <div className="mt-1">
+                <p>{tasksDesc[i].description}</p>
               </div>
             </div>
           ))}
