7febea3484e4b62dede2bad182d19cb0a993c991
fpicco <flor.picco@gmail.com>
2021-12-03 13:17:45 -0300

    Merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -61,10 +61,7 @@ function App(): JSX.Element {
                   onChange={(e) => setNewTask(e.target.value)}
                   value={newTask}
                   ref={taskInput}
-<<<<<<< HEAD
                   placeholder="Title of the task"
-=======
->>>>>>> f2124045b72903a0b7a2258b85d1691f5eb6cd0a
                 />
                 <input
                   className="form-control mt-2"
@@ -72,10 +69,7 @@ function App(): JSX.Element {
                   onChange={(e) => setNewTaskDesc(e.target.value)}
                   value={newTaskDesc}
                   ref={taskInput}
-<<<<<<< HEAD
                   placeholder="Description of the task"
-=======
->>>>>>> f2124045b72903a0b7a2258b85d1691f5eb6cd0a
                 />
                 <button className="btn btn-success col-12 mt-4">Save</button>
               </form>
